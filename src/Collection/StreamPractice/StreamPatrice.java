d9d22711d2dbd031c1d6d83e1190a329b304f77c
SirPhaymuny <163403945+sirphaymuny@users.noreply.github.com>
2024-06-17 11:47:52 +0700

    stream practice

--- src/Collection/StreamPractice/StreamPatrice.java
+++ src/Collection/StreamPractice/StreamPatrice.java
@@ -1,12 +1,15 @@
 package Collection.StreamPractice;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class StreamPatrice {
     public static void main(String[] args) {
         // filter out even number using stream:
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10);
         List<Integer> evenNumber = numbers.stream().filter(num -> num % 2 == 0)
                 .toList();
         System.out.println("even Number : " + evenNumber);
@@ -35,8 +38,53 @@ public class StreamPatrice {
         // Given a list of strings, filter out strings that start with "A", convert them
         // to uppercase, and collect them into a new list
         List<String> strWithA = names.stream().filter(name -> name.startsWith("A"))
-                .map(name->name.toUpperCase()).toList();
+                .map(String::toUpperCase).toList();
         System.out.println("Filtered and uppercase names: " + strWithA);
+
+        //calculate all even number :
+        System.out.println("All number : "+numbers);
+        int evenSum = numbers.stream().filter(num->num%2==0)
+                .reduce(0,Integer::sum);
+        System.out.println("sum of even: "+evenSum);
+
+        //sum all and multiply
+        int averageNum = numbers.stream().reduce(0,Integer::max);
+        System.out.println(averageNum);
+        int multiply = numbers.stream().reduce(1, (a, b) -> a * b);
+        System.out.println(multiply);
+        //
+        List<String> strings = Arrays.asList("a", "b", "c", "d", "e");
+        String concatenated = strings.stream().reduce("", (partialString, element) -> partialString + element);
+        String concatenated1 = strings.stream().reduce("", (s1, s2) -> s1 + s2);
+        System.out.println(concatenated);
+        System.out.println(concatenated1);
+
+        //remove duplicate value using stream
+        //using district to remove duplicate value (make sure your arrayList is sorted too).
+
+        System.out.println("before removed : "+ numbers);
+        List<Integer> removedDuplicated = numbers.stream().sorted().distinct().toList();
+        System.out.println("removed duplicated: "+removedDuplicated);
+
+        //Write a Java program to count the number of strings in a list that start with a specific letter using streams.
+        List <String> colors = Arrays.asList("Red", "Green", "Blue", "Pink", "Brown");
+        String startAt = "B";
+        int countString = colors.stream().filter(name->name.startsWith(startAt))
+                .mapToInt(String::length).sum();
+        System.out.println("couted string of Blue : "+countString);
+
+        //Write a Java program to sort a list of strings in alphabetical order, ascending and descending using streams.
+        List<String> sortedColors = colors.stream().sorted(Comparator.naturalOrder()).toList();
+        System.out.println("ascending : "+sortedColors);
+        List<String> decSorted = colors.stream().sorted(Comparator.reverseOrder()).toList();
+        System.out.println("descending : "+decSorted);
+
+        //Write a Java program to find the second smallest and largest elements in a list of integers using streams.
+        Optional<Integer> number = numbers.stream().distinct().sorted(Comparator.reverseOrder())
+                .skip(1).findFirst();
+        System.out.println("second largest number"+number);
+
+
 
     }
 }
